8778e9774f56a56dc9a7c3490085715a33b0bbc3
Filipe Vieira <filipejmvieira@gmail.com>
2013-12-04 12:51:02 -0200

    Criando cadastro de mercadorias

--- ProjetoFinal/src/controller/Main.java
+++ ProjetoFinal/src/controller/Main.java
@@ -1,5 +1,14 @@
 package controller;
+import views.CadastroMercadoria;
 
 public class Main {
     
+    public static void main(String[] args){
+    
+        CadastroMercadoria c = new CadastroMercadoria();
+        c.setVisible(true);
+    
+     
+        
+    }
 }
--- ProjetoFinal/src/models/Mercadoria.java
+++ ProjetoFinal/src/models/Mercadoria.java
@@ -1,35 +1,55 @@
 package models;
 
-public class Mercadoria extends CustosVariaveis{
-  
-    private String produto;
-    private double preco;
-    private int quantidade;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
-   
+public class Mercadoria {
+  
+        private String produto;
+    public static final String PROP_PRODUTO = "produto";
+
     public String getProduto() {
         return produto;
     }
-    
+
     public void setProduto(String produto) {
+        String oldProduto = this.produto;
         this.produto = produto;
+        propertyChangeSupport.firePropertyChange(PROP_PRODUTO, oldProduto, produto);
+    }
+    private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
     }
 
-    public double getPreco() {
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+    private Double preco;
+    public static final String PROP_PRECO = "preco";
+
+    public Double getPreco() {
         return preco;
     }
 
-    public void setPreco(double preco) {
+    public void setPreco(Double preco) {
+        Double oldPreco = this.preco;
         this.preco = preco;
+        propertyChangeSupport.firePropertyChange(PROP_PRECO, oldPreco, preco);
     }
+    private int quantidade;
+    public static final String PROP_QUANTIDADE = "quantidade";
 
     public int getQuantidade() {
         return quantidade;
     }
 
     public void setQuantidade(int quantidade) {
+        int oldQuantidade = this.quantidade;
         this.quantidade = quantidade;
+        propertyChangeSupport.firePropertyChange(PROP_QUANTIDADE, oldQuantidade, quantidade);
     }
-    
+
     
 }
